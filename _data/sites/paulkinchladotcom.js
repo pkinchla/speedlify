a9507d616837451d4451f38e3c7321ba955fc759
Paul Kinchla <pkinchla@gmail.com>
2022-12-16 14:49:21 -0500

    adding urls

---
+++ _data/sites/paulkinchladotcom.js
@@ -0,0 +1,14 @@
+module.exports = {
+	description: "My personal website.",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://paulkinchla.com",
+		"https://paulkinchla.com/about",
+		"https://paulkinchla.com/work",
+		"https://paulkinchla.com/blog",
+		"https://paulkinchla.com/blog/javascript-still-a-ghost/",
+    "https://paulkinchla.com/work/fixit/"
+	]
+};
