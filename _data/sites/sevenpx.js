3e7d98c59fa00ff88fa90b69250787d2abbe6eeb
Paul Kinchla <pkinchla@gmail.com>
2023-01-28 16:32:26 -0500

    addtion of 7px

---
+++ _data/sites/sevenpx.js
@@ -0,0 +1,8 @@
+module.exports = {
+  description: "Side project about drawing using NextJS",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: ["https://sevenpx.design/", "https://sevenpx.design/drawings/"],
+};
+
