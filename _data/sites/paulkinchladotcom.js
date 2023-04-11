d98150e9b6ecd56325c35499c80091389790d1d2
Paul Kinchla <pkinchla@gmail.com>
2023-04-11 12:23:40 -0400

    updating urls based on migrations

--- _data/sites/paulkinchladotcom.js
+++ _data/sites/paulkinchladotcom.js
@@ -1,14 +1,13 @@
 module.exports = {
-	description: "My personal website.",
-	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
-	},
-	urls: [
-		"https://paulkinchla.com",
-		"https://paulkinchla.com/about/",
-		"https://paulkinchla.com/work/",
-		"https://paulkinchla.com/blog/",
-		"https://paulkinchla.com/blog/javascript-still-a-ghost/",
-    "https://paulkinchla.com/work/fixit/"
-	]
+  description: 'My personal website.',
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+  },
+  urls: [
+    'https://paulkinchla.com',
+    'https://paulkinchla.com/about/',
+    'https://paulkinchla.com/projects/',
+    'https://paulkinchla.com/blog/',
+    'https://paulkinchla.com/blog/javascript-still-a-ghost/',
+  ],
 };
