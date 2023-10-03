0bf16fb9641e0ea5c29dd01f5ac9199d196e9615
Paul Kinchla <pkinchla@gmail.com>
2023-10-02 23:33:39 -0400

    updated frequency for runs to be every other day

--- _data/sites/paulkinchladotcom.js
+++ _data/sites/paulkinchladotcom.js
@@ -1,13 +1,13 @@
 module.exports = {
-  description: 'My personal website.',
+  description: "My personal website.",
   options: {
-    frequency: 60 * 23, // (in minutes), 23 hours
+    frequency: 60 * 47, // (in minutes), 47 hours
   },
   urls: [
-    'https://paulkinchla.com',
-    'https://paulkinchla.com/about/',
-    'https://paulkinchla.com/projects/',
-    'https://paulkinchla.com/blog/',
-    'https://paulkinchla.com/blog/javascript-still-a-ghost/',
+    "https://paulkinchla.com",
+    "https://paulkinchla.com/about/",
+    "https://paulkinchla.com/projects/",
+    "https://paulkinchla.com/blog/",
+    "https://paulkinchla.com/blog/javascript-still-a-ghost/",
   ],
 };
--- _data/sites/sevenpx.js
+++ _data/sites/sevenpx.js
@@ -1,7 +1,7 @@
 module.exports = {
   description: "Side project about drawing using Fresh",
   options: {
-    frequency: 60 * 23, // (in minutes), 23 hours
+    frequency: 60 * 47, // (in minutes), 47 hours
   },
   urls: ["https://sevenpx.design", "https://sevenpx.design/drawings/"],
 };
