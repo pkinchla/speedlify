5d76dc19e639b41fe5234ff6da8710e94e27330a
Paul Kinchla <pkinchla@gmail.com>
2025-08-02 12:44:43 -0400

    bug fixes

--- .eleventy.js
+++ .eleventy.js
@@ -346,7 +346,7 @@ module.exports = function (eleventyConfig) {
   // Assets
   eleventyConfig.addPassthroughCopy({
     "./node_modules/chartist/dist/chartist.js": "chartist.js",
-    "./node_modules/chartist/dist/index.css.map": "chartist.css.map",
+    "./node_modules/chartist/dist/chartist.css.map": "chartist.css.map",
   });
 
   eleventyConfig.addWatchTarget("./assets/");
